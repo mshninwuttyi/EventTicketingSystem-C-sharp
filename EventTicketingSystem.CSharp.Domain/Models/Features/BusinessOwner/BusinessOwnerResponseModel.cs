d79932ffb5d2631bc7c00d92d7874a0eaedf585f
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2025-07-13 13:46:03 +0630

    feat: created business owner response and request model

--- EventTicketingSystem.CSharp.Domain/Models/Features/BusinessOwner/BusinessOwnerResponseModel.cs
+++ EventTicketingSystem.CSharp.Domain/Models/Features/BusinessOwner/BusinessOwnerResponseModel.cs
@@ -7,9 +7,23 @@ public class BusinessOwnerResponseModel
 
 public class BusinessOwnerModel
 {
-    public string BusinessOwnerId { get; set; }
-    public string BusinessOwnerCode { get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public string PhoneNumber { get; set; }
+    public string? Businessownerid { get; set; }
+
+    public string? Businessownercode { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Phonenumber { get; set; }
+
+    public string? Createdby { get; set; }
+
+    public DateTime? Createdat { get; set; }
+
+    public string? Modifiedby { get; set; }
+
+    public DateTime? Modifiedat { get; set; }
+
+    public bool? Deleteflag { get; set; }
 }
