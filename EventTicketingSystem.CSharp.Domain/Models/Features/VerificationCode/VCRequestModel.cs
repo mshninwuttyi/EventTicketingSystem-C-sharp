06cf594031d3fd56d42fe74279af28ab7f84fbf3
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2025-07-21 09:40:37 +0630

    feat: added verification code resquest model

---
+++ EventTicketingSystem.CSharp.Domain/Models/Features/VerificationCode/VCRequestModel.cs
@@ -0,0 +1,14 @@
+﻿namespace EventTicketingSystem.CSharp.Domain.Models.Features.VerificationCode;
+
+public class VCRequestModel
+{
+    public string? Verificationid { get; set; }
+
+    public string? Verificationcode { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Admin { get; set; }
+
+    public bool? Deleteflag { get; set; }
+}
