7e9b0403dd39f2601b7e63815aa27805fcc2af5a
Myat Phone Thant <myatphonethant.dev@gmail.com>
2025-07-21 22:55:51 +0630

    add: jwtservice

---
+++ EventTicketingSystem.CSharp.Api/Services/JwtTokenService.cs
@@ -0,0 +1,11 @@
+﻿namespace EventTicketingSystem.CSharp.Api.Services;
+
+public class JwtTokenService
+{
+
+}
+
+//Step 1 -- API CALL
+//Step 2 -- JWT Token Validation ==> Validate JWT Token (UserId) ==> UserId == AdminUserCode
+//Step 3 -- Controller Action
+//Step 4 -- Business Logic Layer
