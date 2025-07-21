6e8dea8a95d618b36fa635de610d57479a0f9a78
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2025-07-21 09:49:08 +0630

    feat: added verification code response model

--- EventTicketingSystem.CSharp.Domain/Models/Features/VerificationCode/VCResponseModel.cs
+++ EventTicketingSystem.CSharp.Domain/Models/Features/VerificationCode/VCResponseModel.cs
@@ -4,9 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EventTicketingSystem.CSharp.Domain.Models.Features.VerificationCode
+namespace EventTicketingSystem.CSharp.Domain.Models.Features.VerificationCode;
+
+public class VCResponseModel
 {
-    internal class VCResponseModel
-    {
-    }
+    public List<VCodeModel> VerificationCodes {  get; set; }
+    public VCodeModel VerificationCode { get; set; }
+}
+
+public class VCodeModel{
+    public string? Verificationid { get; set; }
+
+    public string? Verificationcode { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Createdby { get; set; }
+
+    public DateTime? Createdat { get; set; }
+
+    public string? Modifiedby { get; set; }
+
+    public DateTime? Modifiedat { get; set; }
+
+    public bool? Deleteflag { get; set; }
 }
