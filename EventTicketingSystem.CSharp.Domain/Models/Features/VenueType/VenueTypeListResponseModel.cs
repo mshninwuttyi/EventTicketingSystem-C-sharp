b5f36b7f0b9467d3c617c9349d595fc3efeded13
LinThantAung16 <linthantag16@gmail.com>
2025-07-26 12:28:44 +0100

    Add Venue Type and fix event category

---
+++ EventTicketingSystem.CSharp.Domain/Models/Features/VenueType/VenueTypeListResponseModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventTicketingSystem.CSharp.Domain.Models.Features.VenueType
+{
+    public class VenueTypeListResponseModel
+    {
+        public List<VenueTypeListModel> VenueTypeList { get; set; }
+    }
+    public class VenueTypeListModel
+    {
+        public string VenueTypeid { get; set; }
+        public string VenueTypeCode { get; set; }
+        public string VenueTypename { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public static VenueTypeListModel FromTblVenueType(TblVenuetype tblVenuetype)
+        {
+            return new VenueTypeListModel
+            {
+                VenueTypeid = tblVenuetype.Venuetypeid,
+                VenueTypeCode = tblVenuetype.Venuetypecode,
+                VenueTypename = tblVenuetype.Venuetypename,
+                CreatedBy = tblVenuetype.Createdby,
+                CreatedAt = tblVenuetype.Createdat
+            };
+        }
+    }
+}
---
+++ EventTicketingSystem.CSharp.Domain/Models/Features/VenueType/VenueTypeUpdateRequestModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventTicketingSystem.CSharp.Domain.Models.Features.VenueType
+{
+    public class VenueTypeUpdateRequestModel
+    {
+        public string VenueTypeCode { get; set; }
+        public string VenueTypeName { get; set; }
+    }
+}
