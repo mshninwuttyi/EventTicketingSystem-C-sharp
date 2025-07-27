558a6eab824a24f3e1503d70b1bbb9d1305303a9
Hnin Wutt Yi <ms.hninwuttyi@gmail.com>
2025-07-27 23:01:11 +0700

    fix: Make only Get and Post method

---
+++ EventTicketingSystem.CSharp.Domain/Models/Features/Ticket/TicketUpdateRequestModel.cs
@@ -0,0 +1,8 @@
+namespace EventTicketingSystem.CSharp.Domain.Models.Features.Ticket;
+
+public class TicketUpdateRequestModel
+{
+    public string? Ticketcode { get; set; }
+    public bool? Isused { get; set; } 
+}
+
