fa990b40caeec86e7db18786e55fd908c062b03c
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2025-07-31 15:55:43 +0630

    feat: dashboard repsonse model created

---
+++ EventTicketingSystem.CSharp.Domain/Models/Features/Dashboard/DashboardResponseModel.cs
@@ -0,0 +1,28 @@
+﻿namespace EventTicketingSystem.CSharp.Domain.Models.Features.Dashboard;
+
+public class DashboardResponseModel
+{
+    public int TotalEvent { get; set; }
+
+    public int TotalVenue { get; set; }
+
+    public int TotalAdmin { get; set; }
+
+    public int TotalBO { get; set; }
+
+    public List<DashboardTicketCount> TicketCounts { get; set; }
+
+    public List<DashboardTicketSale> TicketSales {get; set;}
+}
+
+public class DashboardTicketCount
+{
+    public string Label { get; set; }
+    public int TotalCount { get; set; }
+}
+
+public class DashboardTicketSale
+{
+    public string Month { get; set; }
+    public int TotalCount { get; set; }
+}
