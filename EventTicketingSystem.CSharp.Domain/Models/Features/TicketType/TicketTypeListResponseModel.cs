80e1ea9b9f7458fee55eb06a7f0085c34728f12a
Htet Htet Hlaing <46557012+itsmehtet@users.noreply.github.com>
2025-08-04 15:51:30 +1000

    Update TicketTypeListResponseModel.cs
    Get the correct version of TicketTypeListModel from dev/tickettype branch.

--- EventTicketingSystem.CSharp.Domain/Models/Features/TicketType/TicketTypeListResponseModel.cs
+++ EventTicketingSystem.CSharp.Domain/Models/Features/TicketType/TicketTypeListResponseModel.cs
@@ -5,11 +5,19 @@ namespace EventTicketingSystem.CSharp.Domain.Models.Features.TicketType
         public List<TicketTypeListModel>? TicketTypeList { get; set; }
     }
 
-    public class TicketTypeListModel
-    {
-        public string? Tickettypecode { get; set; }
-
-        public string? Tickettypename { get; set; }
+public class TicketTypeListModel
+{
+    public string? TicketTypeId { get; set; }
+    public string? TicketTypeCode { get; set; }
+    public string? EventCode { get; set; }
+    public string? EventName { get; set; }
+    public string? TicketTypeName { get; set; }
+    public decimal Ticketprice { get; set; }
+    public string? TicketQuantity { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime? CreatedAt { get; set; }
+    public string? ModifiedBy { get; set; }
+    public DateTime? ModifiedAt { get; set; }
 
         public static TicketTypeListModel FromTblTickettype(TblTickettype tblTickettype)
         {
