13528631aa0e347dfbde2dbaeb4b752b88a20891
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2025-08-07 13:25:55 +0630

    feat: add jwt token in swagger ui

--- EventTicketingSystem.CSharp.Api/Program.cs
+++ EventTicketingSystem.CSharp.Api/Program.cs
@@ -3,8 +3,8 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Authorization;
-
-#region Logging
+using Microsoft.OpenApi.Models;
+using Serilog;
 
 string logFolderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs");
 string logFileName = Path.Combine(logFolderPath, "logs_");
@@ -22,22 +22,45 @@ Log.Logger = new LoggerConfiguration()
         rollingInterval: RollingInterval.Hour)
     .CreateLogger();
 
-#endregion
-
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSerilog();
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. Enter your token in the text input below.",
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 builder.Services.AddModularServices(builder);
 
 builder.Services.AddHttpContextAccessor();
 
-
-#region Jwt Token
 
 builder.Services.AddOptions<JwtSettings>()
     .Bind(builder.Configuration.GetSection("JwtSettings"))
@@ -45,8 +68,8 @@ builder.Services.AddOptions<JwtSettings>()
     .Validate(settings => !string.IsNullOrWhiteSpace(settings.SecretKey), "SecretKey must be provided.")
     .ValidateOnStart();
 
-var jwtSettings = builder.Configuration.GetSection("JwtSettings").Get<JwtSettings>() 
-                  ?? throw new InvalidOperationException("JwtSettings configuration is missing or invalid.");
+var jwtSettings = builder.Configuration.GetSection("JwtSettings").Get<JwtSettings>()
+                     ?? throw new InvalidOperationException("JwtSettings configuration is missing or invalid.");
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
@@ -64,8 +87,6 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     });
 
 builder.Services.AddAuthorization();
-
-#endregion
 
 var app = builder.Build();
 
